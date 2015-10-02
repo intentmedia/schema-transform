4bf90364574b311579e786bc7eebb81ca7cf0a79
sebastianrojasvivanco <sebastian.vivanco@intentmedia.com>
2015-10-02 11:37:49 -0400

    Implementing nested and optional records for prismatic->avro transformations

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject com.intentmedia/schema-transform "0.0.1-SNAPSHOT"
   :dependencies [[cheshire "5.5.0"]
                  [prismatic/schema "1.0.0"]
-                 [org.clojure/clojure "1.7.0"]]
+                 [org.clojure/clojure "1.7.0"]
+                 [camel-snake-kebab "0.3.2"]]
   :profiles {:uberjar {:aot :all}}
   :source-paths ["src"]
   :test-paths ["test"])
