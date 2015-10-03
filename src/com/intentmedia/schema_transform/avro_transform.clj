a9f9528903ad9b10a728c42c595fd479e6c203c0
Chet Mancini <chetmancini@gmail.com>
2015-10-02 20:14:33 -0400

    CM: fix complex type formats

--- src/com/intentmedia/schema_transform/avro_transform.clj
+++ src/com/intentmedia/schema_transform/avro_transform.clj
@@ -12,11 +12,8 @@
 ; - Null
 ; - Fixed (as strings)
 ; - Bytes (as strings)
-;
-; Not supported from Avro spec:
-; - Collections of records
 
-(declare avro->prismatic-pair)
+(declare avro-pair->prismatic-pair)
 
 (def avro-primitive->prismatic-primitive
   {"boolean" Boolean
@@ -26,11 +23,8 @@
    "double"  Double
    "string"  String
    "bytes"   String
-   "fixed"   String})
-
-(defn emit-pair [avro value]
-  (let [key (keyword (get avro "name"))]
-    [key value]))
+   "fixed"   String
+   "null"    nil})
 
 (defn is-union? [avro]
   (some #(= "null" %) avro))
@@ -38,63 +32,50 @@
 (defn avro-union->type-str [union]
   (first (remove #(= "null" %) union)))
 
-(defn- avro-nullable->prismatic-nullable [type-field]
-  (let [primitive (avro-union->type-str type-field)]
-    (if (is-union? type-field)
+(defn- avro-nullable->prismatic-nullable [union-field]
+  (let [primitive (avro-union->type-str union-field)]
+    (if (is-union? union-field)
       (s/maybe (avro-primitive->prismatic-primitive primitive)))))
 
-(defn avro-record-transformer [avro]
-  (let [fields (get avro "fields")]
-    (emit-pair avro (reduce (fn [combiner [k v]]
-                              (assoc combiner k v))
-                      {}
-                      (map avro->prismatic-pair fields)))))
+(defn avro-primitive-transformer [union-or-primitive]
+  (if (is-union? union-or-primitive)
+    (avro-nullable->prismatic-nullable union-or-primitive)
+    (avro-primitive->prismatic-primitive union-or-primitive)))
 
-(defn avro-primitive-transformer [avro]
-  (let [type-field (get avro "type")]
-    (if (is-union? type-field)
-      (emit-pair avro (avro-nullable->prismatic-nullable type-field))
-      (emit-pair avro (avro-primitive->prismatic-primitive type-field)))))
+(defn avro-record-transformer [avro-record]
+  (let [fields (get avro-record "fields")]
+    (reduce (fn [combiner [k v]]
+              (assoc combiner k v))
+      {}
+      (map avro-pair->prismatic-pair fields))))
 
-(defn avro-array-transformer [avro]
-  (emit-pair avro [(avro-primitive->prismatic-primitive (get avro "items"))]))
+(defn avro-array-transformer [avro-array-type]
+  (let [item-raw-type (get avro-array-type "items")]
+    [(avro-primitive->prismatic-primitive item-raw-type)]))
 
-(defn avro-enum-transformer [avro]
-  (emit-pair avro (apply s/enum (get avro "symbols"))))
+(defn avro-enum-transformer [avro-enum]
+  (apply s/enum (get avro-enum "symbols")))
 
-(defn avro-null-transformer [avro]
-  "Avro supports null types. Prismatic does not really have this so we'll just return nil."
-  (emit-pair avro nil))
+(defn avro-map-transformer [avro-map-type]
+  (if-let [value-type (avro-primitive->prismatic-primitive (get avro-map-type "values"))]
+    {String value-type}))
 
-(defn avro-map-transformer [avro]
-  "Transform an avro map spec to Prismatic schema map description.
-  While Prismatic supports different key types, the Avro spec assumes string keys so we hardcode that."
-  (if-let [value-type (avro-primitive->prismatic-primitive (get avro "values"))]
-    (emit-pair avro {String value-type})))
+(defn avro-fixed-transformer [avro-fixed-type]
+  String)
 
 (def avro-type->transformer
-  {"record"  avro-record-transformer
-   "boolean" avro-primitive-transformer
-   "int"     avro-primitive-transformer
-   "long"    avro-primitive-transformer
-   "float"   avro-primitive-transformer
-   "double"  avro-primitive-transformer
-   "string"  avro-primitive-transformer
-   "bytes"   avro-primitive-transformer
-   "fixed"   avro-primitive-transformer
-   "array"   avro-array-transformer
-   "enum"    avro-enum-transformer
-   "null"    avro-null-transformer
-   "map"     avro-map-transformer})
+  {"record" avro-record-transformer
+   "array"  avro-array-transformer
+   "enum"   avro-enum-transformer
+   "map"    avro-map-transformer
+   "fixed"  avro-fixed-transformer})
 
-(defn avro->prismatic-pair [avro]
-  (let [raw-type (get avro "type")
-        type (cond
-               (contains? avro-type->transformer raw-type) raw-type
-               (is-union? raw-type) (avro-union->type-str raw-type))
-        transformer (get avro-type->transformer type)]
-    (transformer avro)))
+(defn avro-type-transformer [avro-type]
+  (if (or (contains? avro-primitive->prismatic-primitive avro-type) (is-union? avro-type))
+    (avro-primitive-transformer avro-type)
+    ((get avro-type->transformer (get avro-type "type")) avro-type)))
 
-(defn avro-string->prismatic [avro-json-schema]
-  (let [pair (avro->prismatic-pair (parse-string avro-json-schema))]
-    (last pair)))
+(defn avro-pair->prismatic-pair [avro-pair-map]
+  (let [name (get avro-pair-map "name")
+        value-type (get avro-pair-map "type")]
+    [(keyword name) (avro-type-transformer value-type)]))
--- test/com/intentmedia/schema_transform/avro_transform_test.clj
+++ test/com/intentmedia/schema_transform/avro_transform_test.clj
@@ -1,61 +1,44 @@
 (ns com.intentmedia.schema-transform.avro-transform-test
   (:require [com.intentmedia.schema-transform.avro-transform :refer :all]
-            [clojure.test :refer :all]
-            [schema.core :as s]))
+    [clojure.test :refer :all]
+    [schema.core :as s]))
 
-(def avro-nullable "{\"namespace\":\"example.avro\",\"type\":\"record\",\"name\":\"User\",\"fields\":[{\"name\":\"name\",\"type\":\"string\"},{\"name\":\"favorite_number\",\"type\":[\"int\",\"null\"]},{\"name\":\"favorite_color\",\"type\":[\"string\",\"null\"]}]}")
-
-(def prismatic-nullable
-  {:name            String
-   :favorite_number (s/maybe Integer)
-   :favorite_color  (s/maybe String)})
 
 (deftest test-avro-primitive-transformer
   (testing "Converts a single field"
-    (is (= [:name String] (avro-primitive-transformer {"name" "name" "type" "string"})))
-    (is (= [:name Integer] (avro-primitive-transformer {"name" "name" "type" "int"})))
-    (is (= [:name Double] (avro-primitive-transformer {"name" "name" "type" "double"})))
-    (is (= [:name Long] (avro-primitive-transformer {"name" "name" "type" "long"})))
-    (is (= [:name Boolean] (avro-primitive-transformer {"name" "name" "type" "boolean"})))
-    (is (= [:name String] (avro-primitive-transformer {"name" "name" "type" "fixed"})))
-    (is (= [:name String] (avro-primitive-transformer {"name" "name" "type" "bytes"}))))
+    (is (= String (avro-primitive-transformer "string")))
+    (is (= Integer (avro-primitive-transformer "int")))
+    (is (= Double (avro-primitive-transformer "double")))
+    (is (= Float (avro-primitive-transformer "float")))
+    (is (= Long (avro-primitive-transformer "long")))
+    (is (= Boolean (avro-primitive-transformer "boolean")))
+    (is (= String (avro-primitive-transformer "bytes"))))
   (testing "Converts a union (nullable) field in either order"
-    (is (= [:name (s/maybe Integer)] (avro-primitive-transformer {"name" "name" "type" ["int" "null"]})))
-    (is (= [:name (s/maybe Integer)] (avro-primitive-transformer {"name" "name" "type" ["null" "int"]})))))
-
-(deftest test-avro-enum-transformer
-  (testing "Converts an enum"
-    (is (= [:suits (s/enum "SPADES" "HEARTS" "DIAMONDS" "CLUBS")]
-           (avro-enum-transformer {"name"    "suits"
-                                   "type"    "enum"
-                                   "symbols" ["SPADES" "HEARTS" "DIAMONDS" "CLUBS"]})))))
-
-(deftest test-avro-record-transformer
-  (testing "Converts a record"
-    (is (= [:rec {:name            String
-                  :favorite_number (s/maybe Integer)}]
-           (avro-record-transformer {"name"      "rec"
-                                     "namespace" "example.avro"
-                                     "type"      "record"
-                                     "fields"    [{"name" "name" "type" "string"}
-                                                  {"name" "favorite_number" "type" ["null" "int"]}]})))))
+    (is (= (s/maybe Integer) (avro-primitive-transformer ["int" "null"])))
+    (is (= (s/maybe Integer) (avro-primitive-transformer ["null" "int"])))))
 
 (deftest test-avro-array-transformer
   (testing "Converts an array"
-    (is (= [:arr [String]] (avro-array-transformer {"name"  "arr"
-                                                    "type"  "array"
-                                                    "items" "string"})))))
+    (is (= [String] (avro-array-transformer {"type"  "array"
+                                             "items" "string"})))))
+
+(deftest test-avro-enum-transformer
+  (testing "Converts an enum"
+    (is (= (s/enum "SPADES" "HEARTS" "DIAMONDS" "CLUBS")
+          (avro-enum-transformer {"type"    "enum"
+                                  "symbols" ["SPADES" "HEARTS" "DIAMONDS" "CLUBS"]})))))
 
 (deftest test-avro-map-transformer
   (testing "Converts a map"
-    (is (= [:map-name {String Double}] (avro-map-transformer {"name"   "map-name"
-                                                              "type"   "map"
-                                                              "values" "double"})))))
+    (is (= {String Double} (avro-map-transformer {"type"   "map"
+                                                  "values" "double"})))))
 
-(deftest test-avro-null-transformer
-  (testing "Converts null to nothing"
-    (is (= [:empty nil] (avro-null-transformer {"name" "empty" "type" "null"})))))
-
-(deftest test-avro->prismatic
-  (testing "It correctly parses an avro schema into a Prismatic schema"
-    (is (= prismatic-nullable (avro-string->prismatic avro-nullable)))))
+(deftest test-avro-record-transformer
+  (testing "Converts a record"
+    (is (= {:name            String
+            :favorite_number (s/maybe Integer)}
+          (avro-record-transformer {"name"      "rec"
+                                    "namespace" "example.avro"
+                                    "type"      "record"
+                                    "fields"    [{"name" "name" "type" "string"}
+                                                 {"name" "favorite_number" "type" ["null" "int"]}]})))))
